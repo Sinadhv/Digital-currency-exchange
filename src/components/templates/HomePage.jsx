926563fab543051f520153024f2679fadf53b669
sina dehvari <cnadhv7@gmail.com>
2024-02-01 20:26:05 +0330

    create services
    create services folder for put my API in JavaScript file and use it (clean code)

--- src/components/templates/HomePage.jsx
+++ src/components/templates/HomePage.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { useState } from 'react';
 import { useEffect } from 'react'
+import { getCoinList } from '../../services/CryptoApi';
 import TableCoin from '../modules/TableCoin';
 
 function HomePage() {
@@ -8,9 +9,13 @@ function HomePage() {
 
 
  useEffect (() => {
-  fetch ("https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=20&page=1&x_cg_demo_api_key=CG-etJrkmKtSzx1Bj4o1736skSV")
-  .then(res => res.json())
-  .then((json) => setCoins(json));
+  const getData = async () => {
+   const res = await fetch(getCoinList());
+   const json = await res.json();
+   setCoins(json);
+  };
+
+  getData();
  } , []);
 
 
