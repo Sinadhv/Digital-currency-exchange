7ace428ed7fd47d7cfa394e947a1ccd601d3f033
sina dehvari <cnadhv7@gmail.com>
2024-02-04 17:03:40 +0330

    Add search functionality to the project

--- src/services/CryptoApi.js
+++ src/services/CryptoApi.js
@@ -5,4 +5,7 @@ const getCoinList = (page , currency) => {
 	return (`${BASE_URL}/coins/markets?vs_currency=${currency}&order=market_cap_desc&per_page=20&page=${page}&x_cg_demo_api_key=${API_KEY}`);
 };
 
-export {getCoinList};
+const searchCoin = (query) => 
+`${BASE_URL}/search?query=${query}&x_cg_demo_api_key=${API_KEY}`;
+
+export {getCoinList , searchCoin};
